1640480e65b5e650a5b500b557e875a001bde89c
Varga Botond <btnd.varga@gmail.com>
2026-01-16 15:58:59 +0100

    Testing netlify

---
+++ react-app/netlify/functions/test.js
@@ -0,0 +1,10 @@
+/*import { Pool } from '@neondatabase/serverless';
+var pool = new Pool( { connectionString: process.env.DATABASE_URL } );
+
+export async function handler() {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ok: true }),
+  };
+}
+*/
